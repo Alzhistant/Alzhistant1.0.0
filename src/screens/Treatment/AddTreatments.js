fc693e110beb88642f68c247f3b1bd3b229afbb2
ecarvallo <enriqueimmontemar@gmail.com>
2020-10-19 20:44:25 -0300

    Update AddTreatments.js
    mas comentarios y librerias de firebase

--- src/screens/Treatment/AddTreatments.js
+++ src/screens/Treatment/AddTreatments.js
@@ -2,12 +2,22 @@ import React, { useState } from 'react';
 import { 
 	View, StyleSheet, TextInput, Text, Picker, ScrollView, 
 	Alert, Button  } from 'react-native';
+
+//Librerías para el pickers de fecha y hora
 import DateTimePickerModal from "react-native-modal-datetime-picker";
 import TimePicker from 'react-native-simple-time-picker';
 
+//Conección con las librerias de nuestra base de datos en firebase/firestore
+import { firebaseApp } from '../utils/firebase';
+import firebase from 'firebase/app';
+import "firebase/storage";
+import "firebase/firestore";
+
+const db = firebase.firestore(firebaseApp);
 
 export default function AddTreatments({ navigation }) {
 	
+	//Título y Descripción del tratamiento
 	const [titulo, setTitle] = React.useState('Titulo');
 	const [desc, setDesc] = React.useState('Descripcion');
 	
@@ -17,17 +27,14 @@ export default function AddTreatments({ navigation }) {
 	//Periodicidad del tratamiento
 	const [numTime, setTimeValue] = React.useState('Cada');
 	
-	//Variables pra las fechas
+	//Variables pra las fechas de inicio y final del tratamiento
 	const [isDatePickerVisible, setDatePickerVisibility] = useState(false);
- 
     const showDatePicker = () => {
       setDatePickerVisibility(true);
     };
- 
     const hideDatePicker = () => {
       setDatePickerVisibility(false);
     };
- 
     const handleConfirm = (date) => {
       console.warn("Una Fecha ha sido seleccionada ", date);
       hideDatePicker();
@@ -40,6 +47,8 @@ export default function AddTreatments({ navigation }) {
     return (
 		<ScrollView style={styles.ScrollView}>
 		<View style={styles.formContainer}>
+		
+			//Título
 			<Text>Titulo: </Text>
 			<TextInput 
 				style={styles.input}
@@ -47,6 +56,7 @@ export default function AddTreatments({ navigation }) {
 				onChangeText={(val) => setTitle(val)}
 			/>
 			
+			//Descripción
 			<Text>Descripcion: </Text>
 			<TextInput 
 				style={styles.input2}
@@ -54,6 +64,7 @@ export default function AddTreatments({ navigation }) {
 				onChangeText={(val2) => setDesc(val2)}
 			/>
 			
+			//Tipo de tratamiento
 			<Text>Tipo: </Text>
 			<Picker
 				selectedValue={selectedValue}
@@ -63,9 +74,9 @@ export default function AddTreatments({ navigation }) {
 				<Picker.Item label="Medicamento" value="medicamento" />
 				<Picker.Item label="Ejercicio Físico" value="ejercicio" />
 				<Picker.Item label="Estimulación de Memoria" value="estimulacion" />
-				
 			</Picker>
 			
+			//Fecha de Inicio del tratamiento
 			<Button title="Fecha Inicio Tratamiento" onPress={showDatePicker} />
 			  <DateTimePickerModal
 				isVisible={isDatePickerVisible}
@@ -73,6 +84,7 @@ export default function AddTreatments({ navigation }) {
 				onConfirm={handleConfirm}
 				onCancel={hideDatePicker}
 			  />
+			//Hora de la fecha de inicio del tratamiento
 			<Text>{selectedHours}:{selectedMinutes}</Text>
 			<TimePicker
 			  selectedHours={selectedHours}
@@ -81,6 +93,7 @@ export default function AddTreatments({ navigation }) {
 			  onChange={(minutes) => selectedMinutes}
 			/>
 			
+			//Fecha de Término del tratamiento
 			<Button title="Fecha Fin Tratamiento" onPress={showDatePicker} />
 			  <DateTimePickerModal
 				isVisible={isDatePickerVisible}
@@ -88,6 +101,7 @@ export default function AddTreatments({ navigation }) {
 				onConfirm={handleConfirm}
 				onCancel={hideDatePicker}
 			  />
+			//Hora de la fecha de termino del tratamiento
 			<Text>{selectedHours2}:{selectedMinutes2}</Text>
 			<TimePicker
 			  selectedHours2={selectedHours2}
@@ -96,13 +110,16 @@ export default function AddTreatments({ navigation }) {
 			  onChange={(minutes) => selectedMinutes2}
 			/>
 			
+			//Periodicidad del tratamiento (cada cuanto se hace la actividad del tratamiento)
 			<Text>Periodicidad: </Text>
+			//Cantidad númerica de horas, días o meses
 			<TextInput 
 				style={styles.input}
 				keyboardType = 'numeric'
 				placeholder='Cada'
 				onChangeText={(val3) => setTitle(val3)}
 			/>
+			//Si se hace efectivamente en un intervalo de horas, días o meses
 			<Picker
 				selectedValue={selectedValue}
 				style={styles.input}
@@ -113,6 +130,7 @@ export default function AddTreatments({ navigation }) {
 				<Picker.Item label="Meses" value="mm" />
 			</Picker>
 			
+			//Botones de doble confirmación del tratamiento una vez llenado el formulario
 			<Button
                 title="Confirmar Tratamiento"
                 containerStyle={styles.btnContainer}
