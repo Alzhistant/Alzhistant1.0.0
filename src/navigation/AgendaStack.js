f77c9c13442dec0057fddcf96373ca9f06c8fd17
ecarvallo <enriqueimmontemar@gmail.com>
2020-10-22 23:50:07 -0300

    arreglos 1

--- src/navigation/AgendaStack.js
+++ src/navigation/AgendaStack.js
@@ -1,14 +1,17 @@
 import React from 'react'
 import { createStackNavigator } from '@react-navigation/stack';
 import FormTratamiento from '../screens/Agenda/FormularioTratamiento';
-import Agenda from '..\screens\Agenda\AgendaPrincipal.js';
+import Agenda from '../screens/Agenda/AgendaPrincipal.js';
 
 const Stack = createStackNavigator();
 
 export default function AgendaStack() {
     return (
         <Stack.Navigator>
-            <Stack.Screen name="formtreatment" component={FormTratamiento} options={{ title: "Alzhistant" }}/>
+            <Stack.Screen 
+				name="agenda" 
+				component={Agenda} 
+				options={{ title: "Agenda" }}/>
         </Stack.Navigator>
     )
 }
---
+++ src/screens/Agenda/AgendaPrincipal.js
@@ -0,0 +1,8 @@
+import React from 'react'
+import { StyleSheet, View,Text,TouchableOpacity,Button } from 'react-native';
+
+export default function Agenda( {navigation} ){
+	return (
+		
+	)
+}
