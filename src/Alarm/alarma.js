3d8cd07696cd6b995a0be21e48b35c280b3a6f0e
Lucciano99 <46768386+Lucciano99@users.noreply.github.com>
2020-11-17 23:34:33 -0600

    Update alarma.js

--- src/Alarm/alarma.js
+++ src/Alarm/alarma.js
@@ -10,14 +10,14 @@ import {
 	NativeEventEmitter,
 	NativeModules,
 } from 'react-native';
-*/
+
 import { firebaseApp } from '../utils/firebase'; 
 import ReactNativeAN from 'react-native-alarm-notification';
 import firebase from 'firebase/app';
 //import react from 'react'
 import "firebase/storage";
 import "firebase/firestore";
-import TratamientoClass from '../clasess/TratamientoClass';
+//import TratamientoClass from '../clasess/TratamientoClass';
 import { Component } from 'react';
 //import { View, Text } from "react-native";
 
@@ -28,7 +28,7 @@ const db = firebase.firestore(firebaseApp);
 
 
 export default function obtenerDatosfire(){
-	/*
+	
     const [tratamientos, setTratamientos] = useState(null);
         useEffect( () => {
 
@@ -43,9 +43,14 @@ export default function obtenerDatosfire(){
 
 	}, [])
 	
-	*/
+	
 
 }
+
+
+
+
+
 
 export class AlarmaTratamiento extends Component{
 	constructor(titulo,descripcion,tipo,frecuencia,fecha_inicio, hora_inicio){
@@ -59,22 +64,48 @@ export class AlarmaTratamiento extends Component{
 
 	async metodos(){
 		//Programar alarma futura
-		const alarma= await ReactNativeAN.scheduleAlarm({...AlarmaTratamiento /*fire_base*/})
+		const alarma= await ReactNativeAN.scheduleAlarm({...AlarmaTratamiento})
 		//console.log(alarma); ==> id = 1
 	
 		//Parar la alarma
-		ReactNativeAN.stopAlarmSound();
+		//ReactNativeAN.stopAlarmSound();
 
 		//Envia notificacion en instante
-		ReactNativeAN.sendNotification(AlarmaTratamiento);
+		//ReactNativeAN.sendNotification(AlarmaTratamiento);
 
 
 
-	}
+	//}
 
-}
+//}
 
 
 //const fireDate = ReactNativeAN.parseDate(new Date(Date.now() + 1000));
 //Establece la fecha para que la alarma se active
 
+*/
+
+import { StatusBar } from 'expo-status-bar';
+import React from 'react';
+import { StyleSheet, Text, View } from 'react-native';
+import * as Notifications from 'expo-notifications';
+import * as Permissions from "expo-permissions";
+
+
+
+const getToken = async () => { //Metodo asincrono, obtiene el token del dispositivo 
+  const { status } = await Permissions.getAsync(Permissions.NOTIFICATIONS); 
+  // Se obtiene el estatus cuando el usuario se le pide permiso para enviarle push noti  
+
+  if(status !== "granted"){// Si el estatus es distinto a lo autorizado
+    return;
+  }
+
+  const token = await Notifications.getExpoPushTokenAsync();
+  //Se obtiene el token unico
+  console.log(token);
+
+
+  return token;
+
+};
