b222b0359972482a03c8e85bbcb7c0a9497a9a04
Lucciano99 <46768386+Lucciano99@users.noreply.github.com>
2020-11-25 13:45:12 -0600

    Update App.js

--- App.js
+++ App.js
@@ -39,21 +39,10 @@ RegisterBackgroundTask = async () => {
   }
 }
 
-
 //-------------------------------------------------------------------------------------
 
 
 
-
-
-
-
-
-LogBox.ignoreWarnings(["Setting a timer", 'Animated: `useNativeDriver`']);
-
-if (!global.btoa) {  global.btoa = encode }
-
-if (!global.atob) { global.atob = decode }
 
 // setInterval(function(){ console.log("Hello"); }, 3000);
 // setInterval(function(){ console.log("Bye"); }, 1000);
@@ -61,7 +50,7 @@ if (!global.atob) { global.atob = decode }
 
 
 export default function App() {
-  Notifier();
+  Notifier.default();
   RegisterBackgroundTask();
   return <Logged />;
 }
